b631d4f17a4f723ef1f1778e3e22b4915ad4a450
gaosw <54204224+GAO-sw@users.noreply.github.com>
2025-04-19 10:55:09 +0300

    Update Book.h

--- Book.h
+++ Book.h
@@ -1,27 +1,27 @@
-
+// Book.h
 #pragma once
 
-#include "LibraryItem.h" 
+#include "LibraryItem.h" // Include base class header
 #include <string>
 #include <iostream>
 
-
+// Represents a Book, derived from LibraryItem
 class Book : public LibraryItem {
 public:
-   
+    // --- Constructors ---
     Book();
     Book(int id, const std::string& title, const std::string& author, const std::string& isbn);
 
-
+    // --- Accessors (Getters) ---
     std::string getAuthor() const;
     std::string getISBN() const;
 
-
-
+    // --- Public Methods ---
+    // Override the display method to show book-specific information
     void display() const override;
 
 private:
-
+    // --- Private Members ---
     std::string author;
     std::string isbn;
 };
