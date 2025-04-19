8dc9a99fcc1d6dadd25a05844b01d3a0031c5707
gaosw <54204224+GAO-sw@users.noreply.github.com>
2025-04-19 10:52:19 +0300

    Update Book.cpp

--- Book.cpp
+++ Book.cpp
@@ -1,11 +1,10 @@
-// Book.cpp
 #include "Book.h"
 #include <iostream>
 
-// --- Constructor Implementations ---
+
 
 Book::Book() : LibraryItem(), author("Unknown Author"), isbn("N/A") {
-    // Default constructor calls base constructor and initializes book members
+    
 }
 
 Book::Book(int id_param, const std::string& title_param, const std::string& author_param, const std::string& isbn_param)
@@ -13,10 +12,10 @@ Book::Book(int id_param, const std::string& title_param, const std::string& auth
       author(author_param),
       isbn(isbn_param)
 {
-    // Parameterized constructor initializes base part and book members
+   
 }
 
-// --- Accessor Implementations ---
+
 
 std::string Book::getAuthor() const {
     return author;
@@ -26,11 +25,11 @@ std::string Book::getISBN() const {
     return isbn;
 }
 
-// --- Method Implementations ---
+
 
 void Book::display() const {
-    // Call base class display first
+
     LibraryItem::display();
-    // Add book-specific details
+ 
     std::cout << ", Author: " << author << ", ISBN: " << isbn;
 }
